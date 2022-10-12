29047e9817260878cd355f9576e12035ea374e4a
Bishal Rai <bishal6193@gmail.com>
2022-10-13 03:18:04 +0545

    Simple UI designed for insertion purpose

--- src/App.js
+++ src/App.js
@@ -7,27 +7,37 @@ const URL = 'http://localhost:3001'
 
 function App() {
   const [details, setDetails] = useState([])
-
+  //State variable for reading values
+  const [detail, setDetail] = useState('')
 
   useEffect(() => {
     axios.get(URL)
-    .then((response) => {
-      setDetails(response.data)
-    }).catch(error => {
-      alert(error.response.data.error)
-    })
+      .then((response) => {
+        setDetails(response.data)
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
   }, [])
 
   return (
-    <div>
+    <div style={{ margin: '20px' }}>
       <h2>Teacher's List</h2>
+
+      {/* UI form for insertion */}
+      <form>
+        <label>Add New</label>
+        <input value ={detail} onChange = {e => setDetails(e.target.value)}></input>
+        <button type = 'button' onClick={save}>Save</button>
+      </form>
+
       <ol>
         {details.map(detail => (
-          <li key = {detail.id}>{detail.fname} {detail.lname}</li>
+          <li key={detail.id}>{detail.fname} {detail.lname}</li>
         ))}
       </ol>
     </div>
   );
 }
+
 
 export default App;
