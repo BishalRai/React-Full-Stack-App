6c18be8a7f76121a94cc984276d08e4061dccece
Bishal Rai <bishal6193@gmail.com>
2022-10-13 04:54:25 +0545

    For Insert the data, save function defined

--- src/App.js
+++ src/App.js
@@ -10,6 +10,46 @@ function App() {
   //State variable for reading values
   const [detail, setDetail] = useState('')
 
+
+
+//function for insert
+function save(){
+  // const json = JSON.stringify({fname: detail}{lname:detail})
+  const json = JSON.stringify({fname: detail})
+  axios.post(URL + 'new', json,{
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+  .then((response) => {
+    //Convert stringifyed JSON object back to Javascript object.
+    const addedObject = JSON.parse(json)
+    //Add id returned by the server to object.
+    addedObject.id = response.data.id
+    //Update state variable with newly added data.
+    setDetail(details => [...details, addedObject])
+    //Detail state variable is emptied so user can start adding another task without deletion of previous info on the form
+    setDetail('')
+  }).catch(error => {
+    alert(error.response.data.error)
+  })
+}
+
+
+
+//Function for Deletion
+function remove(id){
+  axios.delete('${URL}delete/${id}')
+  .then(()=>{
+    const newListWithoutRemoved = details.filter((item) => item.id !== id)
+    setDetails(newListWithoutRemoved)
+  }).catch(error =>{
+    alert(error.response.data.error)
+  })
+
+}
+
+
   useEffect(() => {
     axios.get(URL)
       .then((response) => {
@@ -25,9 +65,9 @@ function App() {
 
       {/* UI form for insertion */}
       <form>
-        <label>Add New</label>
+        <label>Add New </label> 
         <input value ={detail} onChange = {e => setDetails(e.target.value)}></input>
-        <button type = 'button' onClick={save}>Save</button>
+        <button type = 'button' onClick={save}> Save </button>
       </form>
 
       <ol>
@@ -38,6 +78,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
