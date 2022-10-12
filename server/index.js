48bc3da4e4a7ccef497bfe068875083bd702b268
Bishal Rai <bishal6193@gmail.com>
2022-10-12 17:15:34 +0545

    Added Get section to index of server folder and instlled sql2 libraries

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 const port = 3001
+//add app get section here
+app.get("/", (req,res) => {
+    res.status(200).json({message: "Node Server is responding."})
+})
 app.listen(port)
